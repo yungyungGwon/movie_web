55772315cc140168b6db6daefcc9c09f918d7f5d
yungyungGwon <gywn7724@gmail.com>
2021-11-11 17:13:26 +0900

    2021.11.11 commit(useEffect ())

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setvalue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+  const onClick = () => setvalue((prev)=> prev + 1);
+
+  useEffect(() => {
+    console.log("I run Only once.");
+  }, []);/*uesEffect는 한번만 실행되도록 하는 함수*/
+  
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    if(keyword !== "" & keyword.length > 5){ 
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);/*[]안에 변수를 넣어주면 값이 변경될때에만 해당 함수가 시행되도록 해줌!*/
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input 
+        value={keyword}
+        onChange={onChange}
+        type="text" 
+        placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
 
 export default App;
+ 
+/*
+NPM은 패키지 관리자이며 NPM을 사용하여 node.js 패키지를 설치할 수 있습니다
+
+NPX는 node.js 패키지를 실행하는 도구입니다.
+*/
