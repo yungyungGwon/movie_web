458dd7a882f2d98f877cf7abf54dc6aa17f539bc
yungyungGwon <gywn7724@gmail.com>
2021-11-12 22:12:31 +0900

    2021.11.12 commit(ToDo list_)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
 function App() {
-  const [counter, setvalue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-  const onClick = () => setvalue((prev)=> prev + 1);
-
-  useEffect(() => {
-    console.log("I run Only once.");
-  }, []);/*uesEffect는 한번만 실행되도록 하는 함수*/
-  
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-
-  useEffect(() => {
-    if(keyword !== "" & keyword.length > 5){ 
-      console.log("SEARCH FOR", keyword);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) =>{
+    event.preventDefault();
+    if(toDo === ""){
+      return;
     }
-  }, [keyword]);/*[]안에 변수를 넣어주면 값이 변경될때에만 해당 함수가 시행되도록 해줌!*/
-  
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  }
   return (
     <div>
-      <input 
-        value={keyword}
-        onChange={onChange}
-        type="text" 
-        placeholder="Search here..." 
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange}
+          value={toDo}
+          type="text" 
+          placeholder="Wirte your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+         {toDos.map((item, index) => 
+          <li key={index}>{item}</li>
+        )}
+      </ul>
     </div>
   );
 }
