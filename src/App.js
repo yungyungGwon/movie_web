9e9ad364bbae32a97548727ee0edf1f76aab82a2
yungyungGwon <gywn7724@gmail.com>
2021-11-14 18:03:20 +0900

    2021.11.14 commit(movie_web 1)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import {useEffect, useState} from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+}from  "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, [])
-  return (
-    <div> 
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <select>
-        {coins.map((coin)=> <option> {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} </option>)}
-      </select>
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path={process.env.PUBLIC_URL + "/"} element={<Home />} ></Route>
+      <Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />} ></Route>
+    </Routes  >
+  </Router>;
 }
-
+ 
 export default App;
  
 /*
 NPM은 패키지 관리자이며 NPM을 사용하여 node.js 패키지를 설치할 수 있습니다
 
 NPX는 node.js 패키지를 실행하는 도구입니다.
-*/
+*/ 
---
+++ src/components/movie.js
@@ -0,0 +1,27 @@
+import PropType from "prop-types";
+import {Link} from "react-router-dom";/* link는 페이지 새로고침없이 전환되는 함수?이다.*/
+
+function Movie({medium_cover_image, title,summary,genres, id}){
+    return (
+        <div>
+        <img src={medium_cover_image} alt={title} />
+        <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+        <p>{summary}</p>
+        <ul>
+            {genres.map((g) =>{
+                <li key={g}>{g}</li>
+            })}
+        </ul>
+    </div>
+    );
+}
+
+Movie.prototype = {
+    medium_cover_image: PropType.string.isRequired,
+    title: PropType.string.isRequired,
+    summary: PropType.string.isRequired,
+    genres: PropType.arrayOf(PropType.string).isRequired,
+    id: PropType.number.isRequired,
+}
+
+export default Movie;
