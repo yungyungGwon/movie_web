25071caaeedbdac6f9d4142626d7801d2f6298f2
yungyungGwon <gywn7724@gmail.com>
2021-11-15 19:09:35 +0900

    2021.11.15 Commit

--- src/components/movie.js
+++ src/components/movie.js
@@ -6,7 +6,7 @@ function Movie({medium_cover_image, title,summary,genres, id}){
         <div>
         <img src={medium_cover_image} alt={title} />
         <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-        <p>{summary}</p>
+        <p>{summary.length>235 ? `${summary.slice(0,235)}...` : summary}</p>
         <ul>
             {genres.map((g) =>{
                 <li key={g}>{g}</li>
